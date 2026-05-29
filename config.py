3ea1fae429624db280c5248de2bc95a3c2317127
MuzamilMM <m.sajjad.29350@khi.iba.edu.pk>
2026-05-29 17:16:15 +0500

    Fix MongoDB SSL issue

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ HISTORICAL_START = "2025-01-01"
 DATA_DIR         = "data"
 HISTORICAL_CSV   = f"{DATA_DIR}/karachi_historical.csv"
 
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://muzamilsthpg_db_user:S0XCnQg5lNVaLwkO@cluster0.gygdlvl.mongodb.net/?appName=Cluster0")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://muzamilsthpg_db_user:S0XCnQg5lNVaLwkO@cluster0.gygdlvl.mongodb.net/?appName=Cluster0&tlsAllowInvalidCertificates=true")
 MONGO_DB  = "aqi_predictor"
 RAW_COLLECTION      = "raw_data"
 FEATURES_COLLECTION = "features"
