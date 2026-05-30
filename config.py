926e624db0b9f160be9b9f63678c824b953593f2
MuzamilMM <m.sajjad.29350@khi.iba.edu.pk>
2026-05-30 19:51:13 +0500

    Fix MongoDB connection

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ HISTORICAL_START    = "2025-01-01"
 DATA_DIR            = "data"
 HISTORICAL_CSV      = f"{DATA_DIR}/karachi_historical.csv"
 
-MONGO_URI           = os.environ["mongodb+srv://msajjad29350_db_user:kYoExAOXy95Z2GEP@cluster0.b44pod8.mongodb.net/?appName=Cluster0"]
+MONGO_URI           = os.environ.get("MONGO_URI", "")
 MONGO_DB            = "aqi_predictor"
 RAW_COLLECTION      = "raw_data"
 FEATURES_COLLECTION = "features"
