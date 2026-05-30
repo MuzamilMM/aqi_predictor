cf188710e69b4e36746fb172afdc07589f92d5d5
MuzamilMM <m.sajjad.29350@khi.iba.edu.pk>
2026-05-30 07:14:10 +0500

    MongoDB with CSV fallback for GitHub Actions

--- config.py
+++ config.py
@@ -8,7 +8,9 @@ HISTORICAL_START = "2025-01-01"
 DATA_DIR         = "data"
 HISTORICAL_CSV   = f"{DATA_DIR}/karachi_historical.csv"
 
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://muzamilsthpg_db_user:S0XCnQg5lNVaLwkO@cluster0.gygdlvl.mongodb.net/?appName=Cluster0&tlsAllowInvalidCertificates=true")
+# Only use MongoDB if MONGO_URI is set as environment variable
+# Locally set this in your terminal: $env:MONGO_URI = "your_connection_string"
+MONGO_URI = os.environ.get("MONGO_URI", "")
 MONGO_DB  = "aqi_predictor"
 RAW_COLLECTION      = "raw_data"
 FEATURES_COLLECTION = "features"
